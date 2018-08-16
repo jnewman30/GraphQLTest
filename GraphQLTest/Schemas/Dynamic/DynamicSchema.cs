76018e9c1bffc9e8421abb042a8249bc2b4fd975
jnewman <jnewman@paperwise.com>
2018-08-16 16:25:07 -0500

    Basic Query Server

---
+++ GraphQLTest/Schemas/Dynamic/DynamicSchema.cs
@@ -0,0 +1,12 @@
+﻿using GraphQL.Types;
+
+namespace GraphQLTest.Schemas.Dynamic
+{
+    public class DynamicSchema : Schema
+    {
+        public DynamicSchema(DynamicQuery query)
+        {
+            Query = query;
+        }
+    }
+}
--- GraphQLTest/Startup.cs
+++ GraphQLTest/Startup.cs
@@ -1,20 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using GraphQL.Server;
+using GraphQL.Server.Ui.Playground;
+using GraphQLTest.Schemas.Dynamic;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace GraphQLTest
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration, IHostingEnvironment environment)
+        {
+            Configuration = configuration;
+            Environment = environment;
+        }
+
+        private IConfiguration Configuration { get; }
+        private IHostingEnvironment Environment { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services
+               .AddSingleton<DynamicQuery>()
+               .AddSingleton<DynamicSchema>();
+
+            // Add GraphQL services and configure options
+            services.AddGraphQL(options =>
+                     {
+                         options.EnableMetrics = true;
+                         options.ExposeExceptions = Environment.IsDevelopment();
+                     })
+                    .AddDataLoader();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +44,10 @@ namespace GraphQLTest
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseGraphQL<DynamicSchema>("/graphql");
+
+            // use graphql-playground middleware at default url /ui/playground
+            app.UseGraphQLPlayground(new GraphQLPlaygroundOptions());
         }
     }
 }
